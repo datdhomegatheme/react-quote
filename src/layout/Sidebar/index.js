64d0ad884456dc54eaf075706b1236cd858c0260
hieudt <thanhhieudo278@gmail.com>
2023-02-10 10:02:12 +0700

    update code

--- src/layout/Sidebar/index.js
+++ src/layout/Sidebar/index.js
@@ -4,7 +4,7 @@ import DashboardPage from "../../pages/DashboardPage";
 import QuoteListPage from "../../pages/QuoteListPage";
 import SettingsPage from "../../pages/SettingsPage";
 import PricingPage from "../../pages/PricingPage";
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 
 function Sidebar() {
     const location = useLocation();
@@ -72,102 +72,18 @@ function Sidebar() {
                 <Route path="/settings">
                     <Route path="general" element={<SettingsPage />} />
 
-    // const toggleMobileNavigationActive = useCallback(
-    //     () =>
-    //         setMobileNavigationActive(
-    //             (mobileNavigationActive) => !mobileNavigationActive
-    //         ),
-    //     []
-    // );
-
-    // const topBarMarkup = (
-    //     <TopBar
-    //         showNavigationToggle
-    //         onNavigationToggle={toggleMobileNavigationActive}
-    //     />
-    // );
-    // const navigate = useNavigate();
-    // const handleClick = () => {
-    //     navigate("/dashboard");
-    // };
-    return (
-        <div className="sidebar">
-            <List>
-                <List.Item>
-                    <Link to="/dashboard" className="nav-link">
-                        Dashboard
-                    </Link>
-                </List.Item>
-                <List.Item>
-                    <Link to="/quote/list" className="nav-link">
-                        Quote List
-                    </Link>
-                </List.Item>
-                <List.Item>
-                    <Link to="/quote-settings" className="nav-link">
-                        Quote Settings
-                    </Link>
-                </List.Item>
-                <List.Item>
-                    <Link to="/settings" className="nav-link">
-                        Settings
-                    </Link>
-                </List.Item>
-                <List.Item>
-                    <Link to="/pricing" className="nav-link">
-                        Pricing Plan
-                    </Link>
-                </List.Item>
-                <List.Item>
-                    <Link to="/recommended-apps" className="nav-link">
-                        Recommended Apps
-                    </Link>
-                </List.Item>
-            </List>
-            {/* topBar={topBarMarkup}
-            showMobileNavigation={mobileNavigationActive}
-            onNavigationDismiss={toggleMobileNavigationActive}
-            navigation=
-            {
-                <div className="sidebar">
-                    <Navigation external location={location.pathname}>
-                        <Navigation.Section
-                            items={[
-                                {
-                                    url: "javascript:void(0)",
-                                    selected:
-                                        location.pathname.trim() ===
-                                        "/dashboard",
-                                    onClick: handleClick,
-                                    label: "Dashboard",
-                                    icon: HomeMinor,
-                                },
-                                {
-                                    url: "/quote/list",
-                                    label: "Quote List",
-                                    icon: DuplicateMinor,
-                                },
-                                {
-                                    url: "/quote-settings/display",
-                                    label: "Quote settings",
-                                    icon: NoteMinor,
-                                },
-                                {
-                                    url: "/settings/general",
-                                    label: "Settings",
-                                    icon: SettingsMinor,
-                                },
-                                {
-                                    url: "/pricing",
-                                    label: "Pricing Plan",
-                                    icon: PlanMinor,
-                                },
-                            ]}
-                        />
-                    </Navigation>
-                </div>
-            } */}
-        </div>
+                    <Route
+                        path="notification&template"
+                        element={<SettingsPage />}
+                    />
+                    <Route path="salesperson">
+                        <Route path="create-salesperson-account" />
+                    </Route>
+                    <Route path="advanced" />
+                </Route>
+                <Route path="/pricing" element={<PricingPage />} />
+            </Routes>
+        </Frame>
     );
 }
 
