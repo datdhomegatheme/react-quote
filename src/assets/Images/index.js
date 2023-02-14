c432f11d0fa1e6f1855d62046a99c9e12dd1c191
hieudt <thanhhieudo278@gmail.com>
2023-02-14 23:25:34 +0700

    update UI

---
+++ src/assets/Images/index.js
@@ -0,0 +1,5 @@
+const Images = {
+    productExample: require("./quoteList/white-background-product-photography-example.png"),
+};
+
+export default Images;
--- src/pages/QuoteListPage/QuoteListDetail/index.js
+++ src/pages/QuoteListPage/QuoteListDetail/index.js
@@ -9,9 +9,11 @@ import {
     Grid,
     Divider,
     ButtonGroup,
+    Image,
 } from "@shopify/polaris";
 import { useCallback, useState } from "react";
 import { SearchMajor } from "@shopify/polaris-icons";
+import Images from "../../../assets/Images";
 
 function QuoteListDetail() {
     //login text field search
@@ -47,76 +49,102 @@ function QuoteListDetail() {
                     </Card.Section>
                 </Card>
 
-                <div className="quote-view-detail__payment">
-                    <Card
-                        title={
-                            <Text variant="headingLg" as="h1">
-                                Payment
-                            </Text>
-                        }
-                    >
-                        <Card.Section>
-                            <table className="payment__table">
-                                <th>
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            Subtotal
-                                        </Text>
-                                    </tr>
-                                    <tr className="content__title">
+                <div className="quote-view-detail__card">
+                    <div className="quote-view-detail__payment">
+                        <Card
+                            title={
+                                <Text variant="headingLg" as="h1">
+                                    Payment
+                                </Text>
+                            }
+                        >
+                            <Card.Section>
+                                <table className="payment__table">
+                                    <th>
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                Subtotal
+                                            </Text>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Button plain>
+                                                <Text variant="bodyLg" as="h1">
+                                                    Add discount
+                                                </Text>
+                                            </Button>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Button plain>
+                                                <Text variant="bodyLg" as="h1">
+                                                    Add shipping
+                                                </Text>
+                                            </Button>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                Total
+                                            </Text>
+                                        </tr>
+                                    </th>
+                                    <th className="payment__content__value">
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                600$
+                                            </Text>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                0$
+                                            </Text>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                0$
+                                            </Text>
+                                        </tr>
+                                        <tr className="content__title">
+                                            <Text variant="bodyLg" as="h1">
+                                                600$
+                                            </Text>
+                                        </tr>
+                                    </th>
+                                </table>
+                            </Card.Section>
+                            <Card.Section>
+                                <div className="payment__buttons">
+                                    <ButtonGroup>
+                                        <Button primary>Send Email</Button>
+                                        <Button>Mark as purchased</Button>
+                                        <Button>Mark as unread</Button>
+                                    </ButtonGroup>
+                                </div>
+                            </Card.Section>
+                        </Card>
+                    </div>
+
+                    <div className="quote-view-detail__card">
+                        <Card title="Viewed Product List">
+                            <Card.Section>
+                                <div className="card__viewed-product-list">
+                                    <img
+                                        alt="white-background-product-photography-example"
+                                        src={Images.productExample}
+                                    />
+                                    <div className="viewed-product-list__content">
                                         <Button plain>
                                             <Text variant="bodyLg" as="h1">
-                                                Add discount
+                                                Product A
                                             </Text>
                                         </Button>
-                                    </tr>
-                                    <tr className="content__title">
-                                        <Button plain>
-                                            <Text variant="bodyLg" as="h1">
-                                                Add shipping
-                                            </Text>
-                                        </Button>
-                                    </tr>
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            Total
+                                        <Text variant="bodyMd" as="h1">
+                                            Date visit: 2022-10-12 13:16:43
+                                            Viewed: 2 times
                                         </Text>
-                                    </tr>
-                                </th>
-                                <th className="payment__content__value">
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            600$
-                                        </Text>
-                                    </tr>
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            0$
-                                        </Text>
-                                    </tr>
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            0$
-                                        </Text>
-                                    </tr>
-                                    <tr className="content__title">
-                                        <Text variant="bodyLg" as="h1">
-                                            600$
-                                        </Text>
-                                    </tr>
-                                </th>
-                            </table>
-                        </Card.Section>
-                        <Card.Section>
-                            <div className="payment__buttons">
-                                <ButtonGroup>
-                                    <Button primary>Send Email</Button>
-                                    <Button>Mark as purchased</Button>
-                                    <Button>Mark as unread</Button>
-                                </ButtonGroup>
-                            </div>
-                        </Card.Section>
-                    </Card>
+                                    </div>
+                                </div>
+                            </Card.Section>
+                        </Card>
+                    </div>
                 </div>
             </Page>
         </div>
